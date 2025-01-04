d67e232408afc38f152fb23c3fdb0177c237ef0f
Ashique A B <ab.ashique10@gmail.com>
2025-01-04 22:19:21 +0530

    added models

---
+++ app/models/user.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String, Integer
+from app.database.connection import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, index=True)
+    email = Column(String, index=True)
